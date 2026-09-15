6f4268bdf9a4eecae39acfff4a12dadeff170b1d
Rehan Hussain <rehanhussain0842@email.com>
2026-09-15 17:36:17 +0530

    plugin added for netlify

--- backend/src/config/elasticsearch.ts
+++ backend/src/config/elasticsearch.ts
@@ -2,4 +2,9 @@ import { Client } from "@elastic/elasticsearch";
 
 export const elasticsearch = new Client({
   node: process.env.ELASTICSEARCH_URL || "http://localhost:9200",
+  auth: process.env.ELASTICSEARCH_API_KEY
+    ? {
+        apiKey: process.env.ELASTICSEARCH_API_KEY,
+      }
+    : undefined,
 });
