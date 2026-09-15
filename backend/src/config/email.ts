cf3e26e22f0bcd599e1301ee2dfffaa9c9e976ef
Rehan Hussain <rehanhussain0842@email.com>
2026-09-15 21:27:24 +0530

    ....

--- backend/src/config/email.ts
+++ backend/src/config/email.ts
@@ -5,8 +5,8 @@ dotenv.config();
 
 export const emailTransporter = nodemailer.createTransport({
   host: process.env.ETHEREAL_HOST,
-  port: Number(process.env.ETHEREAL_PORT || 587),
-  secure: false,
+  port: 465,
+  secure: true,
 
   auth: {
     user: process.env.ETHEREAL_USER,
